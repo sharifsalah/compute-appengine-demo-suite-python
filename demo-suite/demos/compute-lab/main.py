36f1e9039ead17c0a3d010250d1e14a205214573
Patrick Peers <pjpeers@gmail.com>
2014-05-21 19:26:07 +0100

    Create lab now multi instance and began to build classes for monitoring instances

--- demo-suite/demos/compute-lab/main.py
+++ demo-suite/demos/compute-lab/main.py
@@ -9,6 +9,7 @@ import webapp2
 import oauth2client.appengine as oauth2client
 
 from google.appengine.api import users
+from google.appengine.ext import ndb
 
 jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(''))
 oauth_decorator = oauth.decorator
@@ -29,6 +30,14 @@ class Main(webapp2.RequestHandler):
         template = jinja_environment.get_template('demos/compute-lab/templates/index.html')
         self.response.out.write(template.render(variables))
 
+class Instance(ndb.Model):
+    """Data model to record instances"""
+    name = ndb.StringProperty()
+
+class Lab(ndb.Model):
+    name = ndb.StringProperty()
+    instances = ndb.StructuredProperty(Instance, repeated=True)
+
 class CreateLab(webapp2.RequestHandler):
     """Handler for starting a new lab."""
 
@@ -45,10 +54,12 @@ class CreateLab(webapp2.RequestHandler):
     def post(self):
 
         #Get data from form.
+        lab_name = self.request.get('lab-name')
         project_id = self.request.get('project-id')
         lab_zone = self.request.get('lab-zone')
         instance_image = self.request.get('instance-image')
         machine_type = self.request.get('machine-type')
+        number_students = int(self.request.get('total-students'))
 
         #Get credentials stored in datastore
         user_id = users.get_current_user().user_id()
@@ -71,25 +82,59 @@ class CreateLab(webapp2.RequestHandler):
             auto_delete=True,
             init_disk_image=instance_image)]
 
-        #Compile instance object
-        instance = gce.Instance(
-            name="test-instance",
+        #Compile instance objects
+        instances = [gce.Instance(
+            name="%s-%s" % (lab_name, n),
             zone_name=lab_zone,
             machine_type_name=machine_type,
             network_interfaces=networks,
-            disk_mounts=disks)
+            disk_mounts=disks) for n in range(number_students)]
 
         #Send response
         response = gce_appengine.GceAppEngine().run_gce_request(
             self,
-            gce_project.insert,
+            gce_project.bulk_insert,
             'Error inserting instances: ',
-            resource=instance)
+            resources=instances)
 
+        self.redirect('/compute-lab/list')
+
+class ListLab(webapp2.RequestHandler):
+    """Handler for listing lab instances."""
+
+    @oauth_decorator.oauth_required
+    def get(self):
+
+        # project_id = self.request.get('project-id')
+        # lab_zone = self.request.get('lab-zone')
+        project_id = 'grand-century-576'
+        lab_zone = 'europe-west1-b'
+
+        #Configure project object
+        gce_project = gce.GceProject(oauth_decorator.credentials, project_id=project_id, zone_name=lab_zone)
+
+        #Run list request, add desired fields to dictionary
+        instances = gce_appengine.GceAppEngine().run_gce_request(self,
+                                                                 gce_project.list_instances,
+                                                                 'Error listing instances: ',
+                                                                 # filter='name eq ^%s.*' % 'test-instance',
+                                                                 maxResults='500')
+        instance_dict = {}
+        for instance in instances:
+            instance_dict[instance.name] = {'status': instance.status}
+
+
+        variables = {
+            'instances': instance_dict
+        }
+
+        template = jinja_environment.get_template('demos/compute-lab/templates/lab.html')
+        self.response.out.write(template.render(variables))
 
 app = webapp2.WSGIApplication(
     [
         ('/compute-lab', Main),
-        ('/compute-lab/new', CreateLab)
+        ('/compute-lab/new', CreateLab),
+        ('/compute-lab/list', ListLab)
     ],
     debug=True)
