cf9ce54b1d45a1741fc930539f05f9045dc013cb
Patrick Peers <pjpeers@gmail.com>
2014-05-23 18:20:33 +0100

    Updated bootstrap, amended datastore models, add basic instance methods for reporting on instance status

--- demo-suite/demos/compute-lab/main.py
+++ demo-suite/demos/compute-lab/main.py
@@ -1,12 +1,14 @@
 __author__ = 'peersp'
 
 import lib_path
+import logging
 import google_cloud.oauth as oauth
 import google_cloud.gce as gce
 import google_cloud.gce_appengine as gce_appengine
 import jinja2
 import webapp2
 import oauth2client.appengine as oauth2client
+import json
 
 from google.appengine.api import users
 from google.appengine.ext import ndb
@@ -30,13 +32,16 @@ class Main(webapp2.RequestHandler):
         template = jinja_environment.get_template('demos/compute-lab/templates/index.html')
         self.response.out.write(template.render(variables))
 
+class Lab(ndb.Model):
+    """Data model to record labs"""
+    name = ndb.StringProperty()
+    project_id = ndb.StringProperty()
+    lab_zone = ndb.StringProperty()
+
 class Instance(ndb.Model):
     """Data model to record instances"""
     name = ndb.StringProperty()
-
-class Lab(ndb.Model):
-    name = ndb.StringProperty()
-    instances = ndb.StructuredProperty(Instance, repeated=True)
+    lab = ndb.KeyProperty(kind=Lab)
 
 class CreateLab(webapp2.RequestHandler):
     """Handler for starting a new lab."""
@@ -53,7 +58,7 @@ class CreateLab(webapp2.RequestHandler):
     @oauth_decorator.oauth_required
     def post(self):
 
-        #Get data from form.
+        #get data from form.
         lab_name = self.request.get('lab-name')
         project_id = self.request.get('project-id')
         lab_zone = self.request.get('lab-zone')
@@ -61,80 +66,121 @@ class CreateLab(webapp2.RequestHandler):
         machine_type = self.request.get('machine-type')
         number_students = int(self.request.get('total-students'))
 
-        #Get credentials stored in datastore
+        #get credentials stored in datastore
         user_id = users.get_current_user().user_id()
         credentials = oauth2client.StorageByKeyName(
         oauth2client.CredentialsModel, user_id, 'credentials').get()
 
-        #Configure project object
+        #configure project object
         gce_project = gce.GceProject(credentials, project_id=project_id, zone_name=lab_zone)
 
-        #Configure network
+        #configure network
         network = gce.Network('default')
         network.gce_project = gce_project
         networks = [{'accessConfigs': [{'type': 'ONE_TO_ONE_NAT',
                                        'name': 'External NAT'}],
                     'network': network.url}]
 
-        #Configure disks (must be iterable list)
+        #configure disks (must be iterable list)
         disks = [gce.DiskMount(
             boot=True,
             auto_delete=True,
             init_disk_image=instance_image)]
 
-        #Compile instance objects
-        instances = [gce.Instance(
-            name="%s-%s" % (lab_name, n),
-            zone_name=lab_zone,
-            machine_type_name=machine_type,
-            network_interfaces=networks,
-            disk_mounts=disks) for n in range(number_students)]
+        #create lab entity in datastore
+        lab = Lab(name=lab_name,
+                  project_id=project_id,
+                  lab_zone=lab_zone)
+        lab.put()
 
-        #Send response
+        #create instance objects
+        instances = []
+        for n in range(number_students):
+            instances.append(gce.Instance(
+                name="%s-%s" % (lab_name, n),
+                zone_name=lab_zone,
+                machine_type_name=machine_type,
+                network_interfaces=networks,
+                disk_mounts=disks))
+            instance = Instance(name="%s-%s" % (lab_name, n),
+                                lab=lab.key)
+            instance.put()
+
+        #send response
         response = gce_appengine.GceAppEngine().run_gce_request(
             self,
             gce_project.bulk_insert,
             'Error inserting instances: ',
             resources=instances)
 
-        self.redirect('/compute-lab/list')
+        self.redirect('/compute-lab/%s' % lab.key.id())
 
-class ListLab(webapp2.RequestHandler):
-    """Handler for listing lab instances."""
+class LabDetails(webapp2.RequestHandler):
+    """Handler for lab details page."""
 
     @oauth_decorator.oauth_required
-    def get(self):
+    def get(self, lab_id):
 
-        # project_id = self.request.get('project-id')
-        # lab_zone = self.request.get('lab-zone')
-        project_id = 'grand-century-576'
-        lab_zone = 'europe-west1-b'
-
-        #Configure project object
-        gce_project = gce.GceProject(oauth_decorator.credentials, project_id=project_id, zone_name=lab_zone)
-
-        #Run list request, add desired fields to dictionary
-        instances = gce_appengine.GceAppEngine().run_gce_request(self,
-                                                                 gce_project.list_instances,
-                                                                 'Error listing instances: ',
-                                                                 # filter='name eq ^%s.*' % 'test-instance',
-                                                                 maxResults='500')
-        instance_dict = {}
-        for instance in instances:
-            instance_dict[instance.name] = {'status': instance.status}
-
+        #get lab key from url compute-lab/<lab id>
+        lab_name = ndb.Key('Lab', int(lab_id)).get().name
 
         variables = {
-            'instances': instance_dict
+            'lab_id': lab_id,
+            'lab_name': lab_name
         }
 
         template = jinja_environment.get_template('demos/compute-lab/templates/lab.html')
         self.response.out.write(template.render(variables))
 
+class GetInstanceStatus(webapp2.RequestHandler):
+    """Handler for getting lab status"""
+
+    def post(self):
+
+        #get data from request.
+        data = json.loads(self.request.body)
+        lab_id = data['lab_id']
+
+        #create lab key
+        lab_key = ndb.Key('Lab', int(lab_id))
+
+        #get lab entity from datastore
+        lab = lab_key.get()
+        project_id = lab.project_id
+        lab_zone = lab.lab_zone
+
+        #get expected instances from datastore
+        query = Instance.query(Instance.lab == lab_key).fetch()
+
+        #get active instances for project, add to dictionary
+        gce_project = gce.GceProject(oauth_decorator.credentials, project_id=project_id, zone_name=lab_zone)
+        instances = gce_appengine.GceAppEngine().run_gce_request(self,
+                                                                 gce_project.list_instances,
+                                                                 'Error listing instances: ',
+                                                                 # filter='name eq ^%s.*' % 'test-instance',
+                                                                 maxResults='500')
+        status_dict = {}
+        for instance in instances:
+            status_dict[instance.name] = instance.status
+
+        #compare expected instances with active instances and produce list for passing to client side js function
+        instance_list = []
+        for n in range(len(query)):
+            instance_name = query[n].name
+            if instance_name in status_dict:
+                instance_state = status_dict[query[n].name]
+            else:
+                instance_state = "NOT RUNNING"
+            instance_list.append({"name": instance_name,
+                                  "state": instance_state})
+
+        self.response.out.write(json.dumps(instance_list))
+
 app = webapp2.WSGIApplication(
     [
         ('/compute-lab', Main),
         ('/compute-lab/new', CreateLab),
-        ('/compute-lab/list', ListLab)
+        ('/compute-lab/(\d+)', LabDetails),
+        ('/compute-lab/get-status', GetInstanceStatus)
     ],
     debug=True)
